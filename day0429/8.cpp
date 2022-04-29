2caf60562e1eb700c6ebfdefb5b7231b0394575b
sue4869 <sue4869@naver.com>
2022-04-29 19:39:10 +0900

    strcpy 문제 연습 - swap 연습해보기

---
+++ day0429/8.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+//strcpy 사용해보기
+int main() {
+
+	char buf[10] = "ABC";
+	char tt[10] = "KFC";
+	char temp[10];
+
+	strcpy(temp, buf);
+	strcpy(buf, tt);
+	strcpy(tt, temp);
+
+	cout << buf;
+	cout << tt;
+}
